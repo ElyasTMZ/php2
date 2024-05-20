3c0854dd0c532de5abbea84739a95e5837fba82f
elyastmz <elyastmz@outlook.com>
2024-05-20 20:13:09 -0300

    eu tentei

--- atividade1.php
+++ atividade1.php
@@ -1,4 +1,22 @@
 <?php 
+$nome = array("maria", "joao", "fernando", "pedro", "laercio", "fabio", "antonio", "elyas", "nove", "dez");
+$nota = array(7, 7, 7, 8, 9, 8, 6, 10, 5, 8);
+$soma = array_sum($nota);
+$bign = 10;
 
+echo("a media das notas é:".$soma /10.);
 
+function notamaior($nota, $bign){
+	$maior = 0;
+	if ($nota > $bign ) {
+			
+			$maior = $nota;
+			$nota = $bign;
+			$bign = $maior; 
+		}
+
+	return $nota;
+}
+
+echo($nota)
  ?>
